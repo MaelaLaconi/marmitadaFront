e1fcab4264eac339977226962cee0fe05749b3a7
Tabary Mathieu <43238044+TabaryM@users.noreply.github.com>
2021-11-26 22:27:07 +0100

    Ajout de recettes de cuisine Ajout du composant CookBook (pour lister les recettes)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { ReceiptComponent } from './receipt/receipt.component';
+import { CookbookComponent } from './cookbook/cookbook.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    ReceiptComponent
+    ReceiptComponent,
+    CookbookComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/receipt/receipt.type.ts
@@ -0,0 +1,22 @@
+export type ReceiptType = {
+  // Nom du plat
+  name: string;
+  // Description du plat
+  description: string;
+  // Auteur de la recette
+  author: {
+    firstname?: string,
+    lastname?: string,
+    pseudo: string,
+  };
+  // Liste des ingrédients
+  ingredients: string[];
+  // Liste d'étapes pour mener à bien cette recette
+  steps: string[];
+  // Difficulté de la recette
+  difficulty: number;
+  // Temps estimé pour préparer le plat en minute
+  preparationTime: number;
+  // Temps de cuisson en minute
+  cookingTime: number;
+}
